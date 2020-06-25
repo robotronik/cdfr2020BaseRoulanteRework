b42ba35e501f352cbf15f377ecdc213520418a44
XavRobo <xavier.lesage@t-online.de>
2020-06-25 12:03:44 +0200

    timer

--- lowlevel/include/gpio.h
+++ lowlevel/include/gpio.h
@@ -3,4 +3,4 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
-void gpio_setup_pin_af(uint32_t gpio_port, uint16_t gpio_pin, uint8_t gpio_altfun);
+void gpio_setup_pin_af(enum rcc_periph_clken rcc_clken, uint32_t gpio_port, uint16_t gpio_pin, uint8_t gpio_altfun);
---
+++ lowlevel/include/timer.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <libopencm3/stm32/timer.h>
+
+#include <stdint.h>
+
+void timer_setup(enum rcc_periph_clken rcc_clken, uint32_t timer_peripheral, uint32_t prescaler, uint32_t period);
+
+void timer_setup_outputc(uint32_t timer_peripheral, enum tim_oc_id oc_id, enum tim_oc_mode oc_mode, uint32_t oc_value);
