ae86146f07fd2eda17e342f8e9a3986a9cf7bb96
Victor <victor.millory@outlook.fr>
2020-07-02 12:46:55 +0200

    edit comments/doc and add inversion motor

--- lowlevel/include/timer.h
+++ lowlevel/include/timer.h
@@ -21,27 +21,27 @@
 #include <libopencm3/stm32/rcc.h>
 
 /**
- * @brief This function setup an internal timer
+ * @brief This function setup an internal timer with the given parameters
  * 
- * @param rcc_clken    reset and clock control enable for the timer
+ * @param rcc_clken    reset and clock control enable for the timer (clock tree)
  * @param timer_peripheral    timer selected
- * @param prescaler    prescaling the clock. Dividing the sys_clk by a factor
+ * @param prescaler    the input frequency of the timer (sys_clk) is divided by this factor
  * @param period    period of the timer in us
  */
 void timer_setup(enum rcc_periph_clken rcc_clken, uint32_t timer_peripheral, uint32_t prescaler, uint32_t period);
 
 /**
- * @brief This function configure an output channel for the timer
+ * @brief This function configure the output comparator of a channel for the timer specified
  * 
  * @param timer_peripheral    selected timer
- * @param oc_id    selected channel
- * @param oc_mode    different mode used for the timer (here AF_2 for PWM)
+ * @param oc_id    selected channel of the output comparator
+ * @param oc_mode    different mode used for the timer
  * @param oc_value    initial value of the duty cycle
  */
 void timer_setup_output_c(uint32_t timer_peripheral, enum tim_oc_id oc_id, enum tim_oc_mode oc_mode, uint32_t oc_value);
 
 /**
- * @brief This function start the timer
+ * @brief This function starts the given timer
  * 
  * @param timer_peripheral    selected timer
  */
